642431b5014c270283198aa06bc969fd515de65e
Elizabet Golos <radzivilsmety@gmail.com>
2023-05-08 17:28:31 +0300

    18 TESTS

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,7 +2,9 @@ import org.junit.After;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -17,7 +19,7 @@ public class Tests {
     private static WebDriver driver;
     private Map<String, Object> vars;
     JavascriptExecutor js;
-
+private HomePage homePage;
 
     @Before
     public void setUp() {
@@ -26,6 +28,7 @@ public class Tests {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://skillfactory.ru/");
         vars = new HashMap<String, Object>();
+        homePage = new HomePage(driver);
     }
 
     @After
@@ -48,34 +51,40 @@ public class Tests {
 
     @Test
     public void Test1_contacts() {
-        driver.findElement(By.xpath("//*[@id=\"sbs-562561178-1678713806326\"]/div/a")).click();
-        }
 
+        homePage.clickContacts();
+        Assert.assertEquals("Контактная информация",homePage.getContacts());
+
+    }
     @Test
     public void Test2_Free_events() {
-        driver.findElement(By.xpath("//*[@id=\"sbs-562561178-1678713806343\"]/div/a"));
+        homePage.clickFree_events();
+        Assert.assertEquals("Бесплатные мероприятия и материалы",homePage.getFreeEvents());
     }
     @Test
     public void Test3_career_center() {
-        driver.findElement(By.xpath("//*[@id=\"sbs-562561178-1678713806322\"]/div/a")).click();
+        homePage.clickCareerCenter();
+        Assert.assertEquals("Центр карьеры",homePage.getCareerCenter());
     }
     @Test
     public void Test4_media() {
-        driver.findElement(By.xpath("//*[@id=\"sbs-562561178-1678713806329\"]/div/a")).click();
+        homePage.clickMedia();
+        Assert.assertEquals("Честные истории о карьере в IT", homePage.getMedia());
     }
     @Test
     public void Test5_corporativnoye_obuchenye() {
-        driver.findElement(By.xpath("//*[@id=\"sbs-562561178-1678713806333\"]/div/a")).click();
+        homePage.clickEducation();
+        Assert.assertEquals("Развивайте бизнес", homePage.getCorporateEducation());
     }
     @Test
     public void Test6_courses() {
-        driver.findElement(By.xpath("//*[@id=\"rec562561178\"]/div/div/div[20]/a")).click();
+        homePage.clickCourses();
+        Assert.assertEquals("Онлайн-курсы по IT профессиям",homePage.getCourses());
     }
     @Test
-
     public void Test7_sign_up_for_a_course() {
 
-        driver.findElement(By.cssSelector(".tn-elem__5625611781679405234205 > .tn-atom")).click();
+        homePage.Course_Testing();
         {
             WebElement element = driver.findElement(By.cssSelector(".tn-elem__5608766611679951555527 > .tn-atom"));
             Actions builder = new Actions(driver);
@@ -106,13 +115,13 @@ public class Tests {
     }
     @Test
     public void Test8_send_contact (){
-        driver.findElement(By.name("name")).click();
-        driver.findElement(By.name("name")).sendKeys("Голос Элизабет");
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).sendKeys("goloseliz@gmail.com");
-        driver.findElement(By.name("tildaspec-phone-part[]")).click();
-        driver.findElement(By.name("tildaspec-phone-part[]")).sendKeys("(999) 999-99-99");
-        driver.findElement(By.cssSelector("#form562643444 .t-submit")).click();
+       homePage.ClickContacts();
+       homePage.SendName();
+       homePage.ClickEmail();
+       homePage.SendEmail();
+       homePage.ClickPhoneNumber();
+       homePage.SendPhoneNumber();
+       homePage.Submit();
     }
     @Test
     public void Test9_get_consulation ()  {
@@ -131,4 +140,88 @@ public class Tests {
         driver.findElement(By.cssSelector(".t-form__submit:nth-child(9) > .t-submit")).click();
 
     }
+    @Test
+    public void Test10_read_the_history(){
+        homePage.readHistory();
+    }
+    @Test
+    public void Test11_Ask(){
+        homePage.clickContacts();
+        driver.findElement(By.className("t650__inner-col")).click();
+        driver.findElement(By.id("input_1495810359387")).click();
+        driver.findElement(By.id("input_1495810359387")).sendKeys("Элизабет Голос");
+        driver.findElement(By.id("input_1495810354468")).click();
+        driver.findElement(By.id("input_1495810354468")).sendKeys("goloseliz@gmail.com");
+        driver.findElement(By.name("tildaspec-phone-part[]")).click();
+        driver.findElement(By.name("tildaspec-phone-part[]")).sendKeys("(999) 999-99-99");
+        driver.findElement(By.id("input_1658683104957")).click();
+        driver.findElement(By.id("input_1658683104957")).sendKeys("Test");
+        driver.findElement(By.cssSelector("#form472342157 .t-submit")).click();
+    }
+    @Test
+    public void Test12_go_to_media (){
+        homePage.Go_to_media();
+        Assert.assertEquals("Честные истории о карьере в IT", homePage.getMedia());
+    }
+
+    @Test
+    public void Test13_subscription (){
+        homePage.Subscription();
+        driver.findElement(By.cssSelector(".t-input-group_em:nth-child(1) .t-input")).sendKeys("goloseliz@gmail.com");
+        driver.findElement(By.cssSelector(".tn-form__submit:nth-child(6) > .t-submit")).click();
+    }
+    @Test
+    public void Test14_Free_registration() {
+
+        driver.findElement(By.linkText("БЕСПЛАТНО")).click();
+        vars.put("window_handles", driver.getWindowHandles());
+        driver.findElement(By.cssSelector("#rec487651778 .t-card__col:nth-child(2) .t-card__btn")).click();
+        vars.put("win9864", waitForWindow(2000));
+        driver.switchTo().window(vars.get("win9864").toString());
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys("goloseliz@gmail.com");
+        driver.findElement(By.name("tildaspec-phone-part[]")).click();
+        driver.findElement(By.name("tildaspec-phone-part[]")).click();
+        {
+            WebElement element = driver.findElement(By.name("tildaspec-phone-part[]"));
+            Actions builder = new Actions(driver);
+            builder.doubleClick(element).perform();
+        }
+        driver.findElement(By.name("tildaspec-phone-part[]")).sendKeys("(999) 999-99-99");
+        driver.findElement(By.cssSelector("#form473301432 .t-submit")).click();
+    }
+    @Test
+    public void Test15_look_all_materials (){
+      homePage.Look_all_materials();
+    }
+@Test
+    public void Test16_negative_send_contacts(){
+    String Current_message = "Укажите, пожалуйста, корректный email";
+        driver.findElement(By.name("name")).click();
+        driver.findElement(By.name("name")).sendKeys("Элизабет Голос");
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).sendKeys("123456");
+        driver.findElement(By.name("tildaspec-phone-part[]")).click();
+        driver.findElement(By.name("tildaspec-phone-part[]")).sendKeys("(999) 999-99-99");
+        driver.findElement(By.cssSelector("#form562643444 .t-submit")).click();
+    Assert.assertEquals(Current_message,"Укажите, пожалуйста, корректный email");
+        }
+@Test
+    public void Test17_negative_subscription_Email (){
+        homePage.Subscription();
+        driver.findElement(By.cssSelector(".t-input-group_em:nth-child(1) .t-input")).sendKeys("123456");
+        homePage.SubmitSubscription();
+}
+@Test
+    public void Test18_negative_SendContacts_Phone(){
+        String Current_Message = "Укажите, пожалуйста, корректный номер телефона";
+    driver.findElement(By.name("name")).click();
+    driver.findElement(By.name("name")).sendKeys("Элизабет Голос");
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).sendKeys("123456");
+    driver.findElement(By.name("tildaspec-phone-part[]")).click();
+    driver.findElement(By.name("tildaspec-phone-part[]")).sendKeys("(000) 000-00-00");
+    driver.findElement(By.cssSelector("#form562643444 .t-submit")).click();
+    Assert.assertEquals(Current_Message, "Укажите, пожалуйста, корректный номер телефона");
+}
 }
